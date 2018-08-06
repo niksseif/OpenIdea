027b11dcc438d83c0679435a76a89af2e2992008
NikSS <nik.safe@gmail.com>
2018-08-06 10:50:30 -0700

    add users route and specific users route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', (req, res, next) =>
+	knex('users')
+	.then(rows => res.json(rows))
+);
 
+// GET Specific User
+router.get('/:userid', (req, res, next) => {
+  knex('users')
+  .where('id', req.params.userid)
+  .then((data) => {
+    console.log('the specific user', data)
+    res.json(data)
+  })
+})
 module.exports = router;
