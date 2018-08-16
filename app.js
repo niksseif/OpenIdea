702d79bac4f8a8c3706d1b2dbad27e030c9dfc7f
NikSS <nik.safe@gmail.com>
2018-08-15 19:57:58 -0700

    add the .env to the gitignore

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+require('dotenv').config()
 
 //importing the routes into the app
 const indexRouter = require('./routes/index');
