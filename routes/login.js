68cbe400107ab5c1773d8babd89a79ab3b002381
NikSS <nik.safe@gmail.com>
2018-08-19 12:24:59 -0700

    login ,auth

--- routes/login.js
+++ routes/login.js
@@ -10,6 +10,8 @@ router.post('/', (req, res, next) => {
   console.log('now i am in post from the login server side');
   let email = req.body.email
   let password = req.body.password
+  var hashed = bcrypt.hashSync(req.body.password, 8);
+  console.log(email,password,"<<<email and pass before hashing");
   knex('users')
     .where('email', email)
     .first()
@@ -17,7 +19,8 @@ router.post('/', (req, res, next) => {
       console.log(user,"<<<user fro login post route backend")
       if (user) {
       // user bcrypt.compare to input to hashed password in DB
-        let passwordGood = bcrypt.compareSync(password, user.hashed_password)
+        let passwordGood = bcrypt.compareSync(user.password, hashed)
+        console.log(hashed,user.hashed_password,"<<<<this is comparing pass and hash");
 
         // if all good, create token, and attach it as a cookie attached to the response
         if (passwordGood) {
--- seeds/001users.js
+++ seeds/001users.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { name: 'Alis',email:'alis@wonderland.com',image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDVhVGBuazB9bab2M-eH5weR_NFH9vqibisXZ7OdxFnvnB4ytR", hashed_password: '$2b$08$be6vqFCWkEfNV1BK93.LiuZxuX1fJY0NZylXt1F3iZnZrk.NHY6qu'},
+        { name: 'Alis',email:'alis@wonderland.com',image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDVhVGBuazB9bab2M-eH5weR_NFH9vqibisXZ7OdxFnvnB4ytR", hashed_password: '$2b$08$IfDQWzpC2Feke.tALtOElupO3NAIFI7OpJxFDxhmQkIuASTM.Mqyu'},
         { name: 'Nipun',email:'nipun@grandmother.com',image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNwH9DCM75f13HIcF0PmBnDf8o2WVrCHk6haHMCNrTlfkO_iqy", hashed_password: '$2b$08$a0iWX97/CgIcmtmOfPPfHuK4LiAw.2i3xdGx1dgE5bcdAqb0t7WjG'},
         { name: 'Hilary' ,email:'hilray@party.com',image_url: "https://www.w3schools.com/howto/img_avatar2.png", hashed_password: '$2b$08$lU7LYTw6ad50s02j.a6cweQQUA3Y8oMY2BdAbgHBwt3XyfWRKdON6'}
       ]);
